355d1af219faeac2e36c608c718f6a638c00ae2e
Roberto Santalla <roobre@roobre.es>
2022-06-29 18:07:03 +0200

    source: add markdown source
    Here be dragons.

---
+++ changelog/sources/markdown/md.go
@@ -0,0 +1,175 @@
+// Package markdown implements sourcing unreleased entries from https://keepachangelog.com/en/1.0.0/
+package markdown
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/ast"
+	"github.com/gomarkdown/markdown/md"
+	"github.com/gomarkdown/markdown/parser"
+	"github.com/newrelic/release-toolkit/changelog"
+	"github.com/newrelic/release-toolkit/changelog/sources/markdown/headingdoc"
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	unreleasedHeader = "unreleased"
+	heldHeader       = "held"
+)
+
+var supportedTypes = []changelog.EntryType{
+	changelog.TypeBreaking,
+	changelog.TypeSecurity,
+	changelog.TypeEnhancement,
+	changelog.TypeBugfix,
+}
+
+// Markdown is a changelog.Source that produces a changelog.Changelog from a markdown file that resembles the
+// [Keep a changelog](https://keepachangelog.com/en/1.0.0/) format.
+type Markdown struct {
+	reader io.Reader
+}
+
+// New returns a new Markdown source that will consume the specified reader.
+// No reads are performed until Markdown.Changelog is called.
+func New(r io.Reader) Markdown {
+	return Markdown{reader: r}
+}
+
+// Changelog reads the supplied reader into memory and parses the contents.
+// It will return
+func (m Markdown) Changelog() (*changelog.Changelog, error) {
+	buf, err := io.ReadAll(m.reader)
+	if err != nil {
+		return nil, fmt.Errorf("reading markdown from source: %w", err)
+	}
+
+	doc, err := headingdoc.New(parser.New().Parse(buf))
+	if err != nil {
+		return nil, fmt.Errorf("parsing markdown: %w", err)
+	}
+
+	return builder{doc: doc}.build()
+}
+
+// builder is an object which, from a heading doc, can produce a changelog
+type builder struct {
+	doc *headingdoc.Doc
+	cl  *changelog.Changelog
+
+	visited map[*headingdoc.Doc]bool
+}
+
+func (b builder) build() (*changelog.Changelog, error) {
+	b.cl = &changelog.Changelog{}
+
+	unreleased := b.doc.FindOne(unreleasedHeader)
+	if unreleased == nil {
+		log.Debugf("No %q header found, returning empty changelog", unreleasedHeader)
+		return b.cl, nil
+	}
+
+	b.visited = map[*headingdoc.Doc]bool{}
+	log.Tracef("Gathering changelog entries")
+	for _, changeType := range supportedTypes {
+		log.Debugf("Finding headers for type %q under %q header", changeType, unreleasedHeader)
+
+		changeTypeHeaders := unreleased.Find(string(changeType))
+		if len(changeTypeHeaders) == 0 {
+			log.Debugf("No header found for type %v", changeType)
+			continue
+		}
+
+		for _, headerDoc := range changeTypeHeaders {
+			b.entriesFromHeader(headerDoc, changeType)
+		}
+	}
+
+	held := b.doc.FindOne(heldHeader)
+	if held != nil {
+		b.visited[held] = true
+
+		log.Warnf("Found a L%d %q header, marking changelog as held", held.Level, heldHeader)
+		b.cl.Held = true
+	}
+
+	log.Tracef("Collecting any other headers under %q as notes", unreleasedHeader)
+	b.unvisitedAsNotes(unreleased)
+
+	return b.cl, nil
+}
+
+func (b builder) entriesFromHeader(header *headingdoc.Doc, t changelog.EntryType) {
+	if len(header.Content) <= 1 {
+		log.Warnf("Skipping empty %q header", header.Name)
+		return
+	}
+
+	b.visited[header] = true
+
+	log.Debugf("Extracting list items under header %q", header.Name)
+	// First item of the headingDoc content is always the heading itself, so we skip it for parsing.
+	changes := items(header.Content[1:])
+	if len(changes) == 0 {
+		log.Warnf("No list items found under heaser %q", header.Name)
+	}
+
+	for _, change := range changes {
+		b.cl.Changes = append(b.cl.Changes, changelog.Entry{
+			Message: change,
+			Type:    t,
+		})
+	}
+}
+
+func (b builder) unvisitedAsNotes(header *headingdoc.Doc) {
+	notes := strings.Builder{}
+	for _, headerDoc := range header.Children {
+		if b.visited[headerDoc] {
+			continue
+		}
+
+		log.Debugf("Adding unvisited header %q as notes", headerDoc.Name)
+		for _, node := range headerDoc.Content {
+			buf := markdown.Render(node, md.NewRenderer())
+			notes.Write(buf)
+		}
+	}
+
+	b.cl.Notes = notes.String()
+}
+
+// items receives a list of ast.Node, and for those nodes which are lists, returns the list items inside.
+// Nodes which are not lists are ignored.
+func items(content []ast.Node) []string {
+	var itemsStr []string
+
+	for _, node := range content {
+		list, isList := node.(*ast.List)
+		if !isList {
+			log.Debugf("Skipping non-list node")
+			continue
+		}
+
+		for _, listChild := range list.Children {
+			item, isItem := listChild.(*ast.ListItem)
+			if !isItem {
+				log.Warn("Skipping non-list-item child of a list")
+				continue
+			}
+
+			if len(item.Children) != 1 {
+				log.Warn("ListItem has more than 1 children")
+				continue
+			}
+
+			buf := markdown.Render(item.Children[0], md.NewRenderer())
+			itemsStr = append(itemsStr, strings.TrimSpace(string(buf)))
+		}
+	}
+
+	return itemsStr
+}
---
+++ changelog/sources/markdown/md_test.go
@@ -0,0 +1,135 @@
+package markdown_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	cl "github.com/newrelic/release-toolkit/changelog"
+	"github.com/newrelic/release-toolkit/changelog/sources/markdown"
+)
+
+//nolint:funlen
+func TestMarkdown_Changelog(t *testing.T) {
+	t.Parallel()
+
+	// THIS TEST CARES ABOUT ORDER!
+	// Ensure that expected entries are sorted as per supportedTypes.
+	for _, tc := range []struct {
+		name     string
+		markdown string
+		expected *cl.Changelog
+	}{
+		{
+			name: "Parses_Full_Changelog",
+			markdown: strings.TrimSpace(`
+# Changelog
+This is based on blah blah blah
+
+## Unreleased
+
+### Important announcement (note)
+This is a release note
+
+### Breaking
+- Support has been removed
+
+### Enhancements
+- Added this
+- Improved that
+
+### Bugfixes
+- Fixed a bug that caused the world to end
+- Fixed a bug that rm -rf'd /
+
+### Security
+- Fixed a security issue that leaked all data
+
+## v1.2.3 - 20YY-DD-MM
+
+### Enhancements
+- This is in the past and should not be included
+`),
+			expected: &cl.Changelog{
+				Changes: []cl.Entry{
+					{Type: cl.TypeBreaking, Message: "Support has been removed"},
+					{Type: cl.TypeSecurity, Message: "Fixed a security issue that leaked all data"},
+					{Type: cl.TypeEnhancement, Message: "Added this"},
+					{Type: cl.TypeEnhancement, Message: "Improved that"},
+					{Type: cl.TypeBugfix, Message: "Fixed a bug that caused the world to end"},
+					{Type: cl.TypeBugfix, Message: "Fixed a bug that rm -rf'd /"},
+				},
+				Notes: "### Important announcement (note)\nThis is a release note\n",
+			},
+		},
+		{
+			name: "Parses_Held_L2_Header",
+			markdown: strings.TrimSpace(`
+# Changelog
+This is based on blah blah blah
+
+## Held
+
+Holding release as it contains massive breaking changes
+
+## Unreleased
+
+### Breaking
+- Support has been removed
+
+## v1.2.3 - 20YY-DD-MM
+
+### Enhancements
+- This is in the past and should not be included
+`),
+			expected: &cl.Changelog{
+				Held: true,
+				Changes: []cl.Entry{
+					{Type: cl.TypeBreaking, Message: "Support has been removed"},
+				},
+			},
+		},
+		{
+			name: "Parses_Held_L3_Header",
+			markdown: strings.TrimSpace(`
+# Changelog
+This is based on blah blah blah
+
+## Unreleased
+
+### Held
+
+Holding release as it contains massive breaking changes
+
+### Breaking
+- Support has been removed
+
+## v1.2.3 - 20YY-DD-MM
+
+### Enhancements
+- This is in the past and should not be included
+`),
+			expected: &cl.Changelog{
+				Held: true,
+				Changes: []cl.Entry{
+					{Type: cl.TypeBreaking, Message: "Support has been removed"},
+				},
+			},
+		},
+	} {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			src := markdown.New(strings.NewReader(tc.markdown))
+			chl, err := src.Changelog()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if diff := cmp.Diff(tc.expected, chl); diff != "" {
+				t.Fatalf("Changelog is not the expected one:\n%v", diff)
+			}
+		})
+	}
+}
