e7b45865c4bc052e6436fd7c4d77da8a3dd338c4
Roberto Santalla <roobre@roobre.es>
2022-07-06 18:49:14 +0200

    pretty: add pretty renderer

---
+++ changelog/sources/markdown/pretty/pretty.go
@@ -0,0 +1,53 @@
+package pretty
+
+import (
+	"io"
+
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/ast"
+)
+
+// Renderer is a markdown.Renderer wrapper that adds empty newlines after and before certain elements.
+type Renderer struct {
+	markdown.Renderer
+}
+
+// New returns a new Renderer.
+// TODO: It would be great to be able to configure this to have customizable "before" and "after" spacing.
+func New(inner markdown.Renderer) Renderer {
+	return Renderer{
+		Renderer: inner,
+	}
+}
+
+// RenderNode intercepts the inner renderer's RenderNode, implementing the newline logic.
+func (p Renderer) RenderNode(w io.Writer, node ast.Node, entering bool) ast.WalkStatus {
+	switch {
+	case entering && p.spaceBefore(node):
+		fallthrough
+	case !entering && p.spaceAfter(node):
+		_, _ = io.WriteString(w, "\n")
+	}
+
+	return p.Renderer.RenderNode(w, node, entering)
+}
+
+// spaceAfter returns true if an empty newline should be printer after a node.
+func (p Renderer) spaceAfter(node ast.Node) bool {
+	switch node.(type) {
+	case *ast.List:
+		return true
+	}
+
+	return false
+}
+
+// spaceBefore returns true if an empty newline should be printer before a node.
+func (p Renderer) spaceBefore(node ast.Node) bool {
+	switch h := node.(type) {
+	case *ast.Heading:
+		return h.Level > 1
+	}
+
+	return false
+}
---
+++ changelog/sources/markdown/pretty/pretty_test.go
@@ -0,0 +1,51 @@
+package pretty_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/md"
+	"github.com/gomarkdown/markdown/parser"
+	"github.com/google/go-cmp/cmp"
+	"github.com/newrelic/release-toolkit/changelog/sources/markdown/pretty"
+)
+
+func TestRenderer_RenderNode(t *testing.T) {
+	orig := `# I'm a markdown document
+Here's a pragraph
+## I don't have much spaces
+As you can see
+### And that makes me
+## Incredibly unreadable
+## Dontyathink
+- Heck yes
+
+I think so
+`
+
+	expected := `# I'm a markdown document
+Here's a pragraph
+
+## I don't have much spaces
+As you can see
+
+### And that makes me
+
+## Incredibly unreadable
+
+## Dontyathink
+- Heck yes
+
+I think so
+`
+
+	root := markdown.Parse([]byte(orig), parser.New())
+
+	buf := &strings.Builder{}
+	buf.Write(markdown.Render(root, pretty.New(md.NewRenderer())))
+
+	if diff := cmp.Diff(expected, buf.String()); diff != "" {
+		t.Fatalf("Output markdown is not as expected:\n%s", diff)
+	}
+}
