8e6c0d800deffda51920fd6e49b2686c798058b9
Alvaro Cabanas <acabanas@newrelic.com>
2022-07-11 10:28:54 +0200

    Add Author to commits

--- commit/source.go
+++ commit/source.go
@@ -3,9 +3,10 @@ package commit
 type Commit struct {
 	Message string
 	Hash    string
+	Author  string
 }
 
 // Source returns an ordered list of all following commits to a provided lastHash from a repository
 type Source interface {
-	Commits(lastHash string) ([]string, error)
+	Commits(lastHash string) ([]Commit, error)
 }
