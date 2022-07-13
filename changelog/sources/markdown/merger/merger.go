a3277f77a7b75dd2b62825de5dfa3c13ba0388f4
Roberto Santalla <roobre@users.noreply.github.com>
2022-07-13 15:41:21 +0200

    merger: add (#22)
    * merger: add
    merger: merge markdown as text rather than as an ast
    
    * merger: clarify comments a bit
    
    * merger: handle empty changelogs better
    
    * merger: catch all io errors

---
+++ changelog/sources/markdown/merger/merger.go
@@ -0,0 +1,134 @@
+// Package merger contains the Merger object, which incorporates a changelog.Changelog
+// into a full markdown changelog.
+package merger
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"regexp"
+	"time"
+
+	"github.com/Masterminds/semver"
+	"github.com/newrelic/release-toolkit/changelog"
+	"github.com/newrelic/release-toolkit/changelog/renderer"
+	log "github.com/sirupsen/logrus"
+)
+
+// Merger is an object that can incorporate a changelog.Changelog (section) into an existing CHANGELOG.md document.
+type Merger struct {
+	// ReleasedOn is a function that returns the date in which the new section was released. It defaults to time.Now.
+	ReleasedOn func() time.Time
+
+	// version holds the in which the new changelog was released.
+	version *semver.Version
+	// ch is the already populated, partial changelog containing the latest changes.
+	ch *changelog.Changelog
+}
+
+// New creates a Merger that will integrate the supplied changelog.Changelog into a full Markdown document that contains
+// changelogs for older versions. The inserted section will be marked as being the changelog for the version specified
+// in the supplied semver.Version, and also marked as released on the date returned by Merger.ReleasedOn.
+// Merger is an immutable object and does not modify the changelog.Changelog object, nor the original document supplied
+// to Merge.
+func New(ch *changelog.Changelog, newVersion *semver.Version) Merger {
+	return Merger{
+		ReleasedOn: time.Now,
+		ch:         ch,
+		version:    newVersion,
+	}
+}
+
+var (
+	unreleasedHeader = regexp.MustCompile(`(?i)^##\s*unreleased`)
+	heldHeader       = regexp.MustCompile(`(?i)^##\s*held`)
+	l2Header         = regexp.MustCompile(`^##\s*\w`)
+)
+
+// Merge uses the configured changelog and version to read the current, full changelog in Markdown format from
+// srcChangelog, and write to dst a new full changelog containing the entries specified in the changelog.Changelog
+// object that was passed to New.
+// The source file is left intact and the changelog.Changelog object supplied to New are not modified.
+//nolint:gocyclo,cyclop // Unreasonable amount of iferrnils raise complexity unfairly.
+func (m Merger) Merge(srcChangelog io.Reader, dst io.Writer) error {
+	newSection := &bytes.Buffer{}
+
+	rdr := renderer.New(m.ch)
+	rdr.Next = m.version
+	rdr.ReleasedOn = m.ReleasedOn
+
+	err := rdr.Render(newSection)
+	if err != nil {
+		return fmt.Errorf("rendering new changelog: %w", err)
+	}
+	newSection.WriteString("\n")
+
+	scanner := bufio.NewScanner(srcChangelog)
+
+	// Ignore acts like a sort-of state-machine that tells the consuming loop whether the subsequent line needs to be
+	// copied over or ignored.
+	// Ignore is set to true when a specific L2 header we want to ignore is found, and set back to false when any other
+	// L2 header is found.
+	ignore := false
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		switch {
+		case unreleasedHeader.MatchString(line):
+			log.Tracef("Unreleased header found, printing header and empty section")
+			ignore = true
+
+			_, ioErr := fmt.Fprintf(dst, "%s\n\n", line)
+			if ioErr != nil {
+				return fmt.Errorf("writing changelog: %w", err)
+			}
+
+		case heldHeader.MatchString(line):
+			log.Tracef("Held header found, ignoring both header and section")
+			ignore = true
+
+		case l2Header.MatchString(line):
+			log.Tracef("L2 header found, including section")
+			ignore = false
+
+			// Copy the new section now before writing this header.
+			// Calls to this function on subsequent L2 headers are noop as the newSection buffer is already consumed.
+			written, ioErr := io.Copy(dst, newSection)
+			if ioErr != nil {
+				return fmt.Errorf("writing changelog: %w", err)
+			}
+
+			// If we just wrote something before a header, we add an extra newline so the header is not cuddled with the
+			// newly added section.
+			if written > 0 {
+				_, ioErr = fmt.Fprintln(dst, "")
+				if ioErr != nil {
+					return fmt.Errorf("writing changelog: %w", err)
+				}
+			}
+		}
+
+		if !ignore {
+			_, ioErr := fmt.Fprintln(dst, line)
+			if ioErr != nil {
+				return fmt.Errorf("writing changelog: %w", err)
+			}
+		}
+	}
+
+	if ioErr := scanner.Err(); err != nil {
+		return fmt.Errorf("reading header: %w", ioErr)
+	}
+
+	// If the changelog did not contain any existing L2 header other than Unreleased and Held, newSection will not have
+	// been inserted, so we copy it now again.
+	// If an L2 header existed, this is noop as the newSection reader has already been consumed.
+	_, ioErr := io.Copy(dst, newSection)
+	if ioErr != nil {
+		return fmt.Errorf("writing changelog: %w", err)
+	}
+
+	return nil
+}
---
+++ changelog/sources/markdown/merger/merger_test.go
@@ -0,0 +1,270 @@
+package merger_test
+
+import (
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/Masterminds/semver"
+	"github.com/google/go-cmp/cmp"
+	"github.com/newrelic/release-toolkit/changelog"
+	"github.com/newrelic/release-toolkit/changelog/sources/markdown/merger"
+)
+
+// brokenWristwatch gives a correct time twice a day.
+func brokenWristwatch() time.Time {
+	t, _ := time.Parse("2006-01-02", "1993-09-21")
+	return t
+}
+
+//nolint:funlen // This is a test.
+func TestMerger_Merges(t *testing.T) {
+	t.Parallel()
+
+	// A changelog with nearly every possible section.
+	fullChangelog := changelog.Changelog{
+		Notes: "### Extra\nWe worked really hard on this one, we hope you like it!\n",
+		Changes: []changelog.Entry{
+			{Type: changelog.TypeBugfix, Message: "Fixed this"},
+			{Type: changelog.TypeSecurity, Message: "Leaked that"},
+			{Type: changelog.TypeBreaking, Message: "Broken everything"},
+			{Type: changelog.TypeEnhancement, Message: "Added feature"},
+		},
+		Dependencies: []changelog.Dependency{
+			{Name: "thanklessly-maintained"},
+		},
+	}
+
+	// Simple changelog with just one entry.
+	simpleChangelog := changelog.Changelog{
+		Changes: []changelog.Entry{
+			{Type: changelog.TypeBugfix, Message: "Fixed this"},
+		},
+	}
+
+	for _, tc := range []struct {
+		name     string
+		ch       changelog.Changelog
+		original string
+		expected string
+	}{
+		{
+			name: "Full_Changelog",
+			ch:   fullChangelog,
+			original: strings.TrimSpace(`
+# Changelog
+This is based on blah blah blah
+
+## Unreleased
+
+### Bugfixes
+- This is a section
+- That will be deleted
+
+### Security
+- Completely wiped down from earth
+
+### This also
+
+Will be wiped
+
+## v1.2.3 - 20YY-DD-MM
+
+### Enhancements
+- This is in the past and should be preserved
+			`) + "\n",
+			expected: strings.TrimSpace(`
+# Changelog
+This is based on blah blah blah
+
+## Unreleased
+
+## v1.2.4 - 1993-09-21
+
+### Extra
+We worked really hard on this one, we hope you like it!
+
+### ⚠️️ Breaking changes ⚠️
+- Broken everything
+
+### 🛡️ Security notices
+- Leaked that
+
+### 🚀 Enhancements
+- Added feature
+
+### 🐞 Bug fixes
+- Fixed this
+
+### ⛓️ Dependencies
+- Updated thanklessly-maintained
+
+## v1.2.3 - 20YY-DD-MM
+
+### Enhancements
+- This is in the past and should be preserved
+			`) + "\n",
+		},
+		{
+			name: "Simple_Changelog_With_Held",
+			ch:   simpleChangelog,
+			original: strings.TrimSpace(`
+# Changelog
+This is based on blah blah blah
+
+## Held
+
+This release should be held from automatic releasing as it includes heavy breaking changes
+
+## Unreleased
+
+### Bugfixes
+- This is a section
+- That will be deleted
+
+### Security
+- Completely wiped down from earth
+
+### This also
+
+Will be wiped
+
+## v1.2.3 - 20YY-DD-MM
+
+### Enhancements
+- This is in the past and should be preserved
+			`) + "\n",
+			expected: strings.TrimSpace(`
+# Changelog
+This is based on blah blah blah
+
+## Unreleased
+
+## v1.2.4 - 1993-09-21
+
+### 🐞 Bug fixes
+- Fixed this
+
+## v1.2.3 - 20YY-DD-MM
+
+### Enhancements
+- This is in the past and should be preserved
+			`) + "\n",
+		},
+		{
+			name: "Simple_Changelog_First_Entry",
+			ch:   simpleChangelog,
+			original: strings.TrimSpace(`
+# Changelog
+This is based on blah blah blah
+
+## Unreleased
+
+### Bugfixes
+- This is a section
+- That will be deleted
+
+### Security
+- Completely wiped down from earth
+			`) + "\n",
+			expected: strings.TrimSpace(`
+# Changelog
+This is based on blah blah blah
+
+## Unreleased
+
+## v1.2.4 - 1993-09-21
+
+### 🐞 Bug fixes
+- Fixed this
+			`) + "\n",
+			// TODO: If no previous sections existed in the changelog, there will be two newlines at the end.
+		},
+		{
+			name: "Simple_Changelog_No_Previous_With_Unreleased",
+			ch:   simpleChangelog,
+			original: strings.TrimSpace(`
+# Changelog
+This is based on blah blah blah
+
+## Unreleased
+			`) + "\n",
+			expected: strings.TrimSpace(`
+# Changelog
+This is based on blah blah blah
+
+## Unreleased
+
+## v1.2.4 - 1993-09-21
+
+### 🐞 Bug fixes
+- Fixed this
+			`) + "\n",
+		},
+		{
+			name: "Simple_Changelog_No_Unreleased",
+			ch:   simpleChangelog,
+			original: strings.TrimSpace(`
+# Changelog
+This is based on blah blah blah
+
+## v1.2.3 - 20YY-DD-MM
+
+### Enhancements
+- This is in the past and should be preserved
+			`) + "\n",
+			expected: strings.TrimSpace(`
+# Changelog
+This is based on blah blah blah
+
+## v1.2.4 - 1993-09-21
+
+### 🐞 Bug fixes
+- Fixed this
+
+## v1.2.3 - 20YY-DD-MM
+
+### Enhancements
+- This is in the past and should be preserved
+			`) + "\n",
+		},
+		{
+			// This is an edge case where Merger does not behave extremely well.
+			// Merger cannot easily handle whether before the place where the new section will be inserted, so we took
+			// the decision to assume there will be a space before.
+			// This assumption does not hold if there are neither previous entries, nor an "Unreleased" header.
+			name: "Simple_Changelog_No_Previous_Without_Unreleased",
+			ch:   simpleChangelog,
+			original: strings.TrimSpace(`
+# Changelog
+This is based on blah blah blah
+`) + "\n",
+			expected: strings.TrimSpace(`
+# Changelog
+This is based on blah blah blah
+## v1.2.4 - 1993-09-21
+
+### 🐞 Bug fixes
+- Fixed this
+			`) + "\n",
+		},
+	} {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			buf := &strings.Builder{}
+			mrg := merger.New(&tc.ch, semver.MustParse("v1.2.4"))
+			mrg.ReleasedOn = brokenWristwatch
+			err := mrg.Merge(strings.NewReader(tc.original), buf)
+			if err != nil {
+				t.Fatalf("Merger returned error: %v", err)
+			}
+
+			actual := buf.String()
+			if diff := cmp.Diff(tc.expected, actual); diff != "" {
+				t.Fatalf("Merger did not return the expected changelog:\n%s", diff)
+			}
+		})
+	}
+}
