f5e9a07d9422cca013f6bd2f72940db2bba2d8b7
Roberto Santalla <roobre@roobre.es>
2022-08-16 14:25:44 +0200

    dependabot: small tweaks to commit regex

--- dependabot/source.go
+++ dependabot/source.go
@@ -10,7 +10,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var commitRegex = regexp.MustCompile(`[Bb]ump ([\w-/.]+)(?: from )?(\S+)?(?: to )?(\S+)?(?: \(#(\S+)\))?`)
+var commitRegex = regexp.MustCompile(`[Bb]ump (\S+)(?: from (\S+))?(?: to (\S+))?(?: \([#!](\d+)\))?`)
 
 type Source struct {
 	tagsVersionGetter git.TagsVersionGetter
