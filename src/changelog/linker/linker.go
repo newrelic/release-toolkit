9552ecd3f505ed67f3f17b35fbdf1545460f5a4e
alvarocabanas <acabanas@newrelic.com>
2022-09-08 09:38:42 +0200

    Linker with dictionary and github mappers

---
+++ src/changelog/linker/linker.go
@@ -0,0 +1,59 @@
+// Package linker implements linking dependency changelogs when they get updated.
+package linker
+
+import (
+	"github.com/newrelic/release-toolkit/changelog"
+	log "github.com/sirupsen/logrus"
+)
+
+// Linker is an object containing a map of Mappers that will be applied to Map a changelog to a dependency.
+type Linker struct {
+	Mappers []Mapper
+}
+
+// Mapper is any object that can link to a changelog for a dependency.
+type Mapper interface {
+	Map(dep changelog.Dependency) string
+}
+
+func New(mappers ...Mapper) Linker {
+	return Linker{
+		Mappers: mappers,
+	}
+}
+
+// Link will try to Link all the dependencies in a changelog.yml to the changelogs found in the mappers.
+func (l Linker) Link(cl *changelog.Changelog) error {
+	for i := range cl.Dependencies {
+		dep := &cl.Dependencies[i]
+
+		if dep.Changelog != "" {
+			log.Debugf("Skipping changelog linking for %q as it is already linked to %q", dep.Name, dep.Changelog)
+			continue
+		}
+
+		if dep.To == nil {
+			log.Debugf("Skipping changelog linking for %q as new version is unknown", dep.Name)
+			continue
+		}
+
+		link := l.Map(*dep)
+		if link != "" {
+			dep.Changelog = link
+		}
+	}
+
+	return nil
+}
+
+// Map will iterate all the mappers to map dependencies with their changelogs.
+func (l Linker) Map(dep changelog.Dependency) string {
+	for _, mapper := range l.Mappers {
+		link := mapper.Map(dep)
+		if link != "" {
+			return link
+		}
+	}
+
+	return ""
+}
---
+++ src/changelog/linker/mapper/dictionary.go
@@ -0,0 +1,92 @@
+package mapper
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+	"text/template"
+
+	"github.com/newrelic/release-toolkit/changelog"
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v3"
+)
+
+var ErrNilTemplateField = errors.New("required link template field is nil")
+
+// Dictionary stores a mapping between dependency names and templates to obtain a changelog link.
+// Templates have access to fields of the changelog.Dependency object.
+type Dictionary struct {
+	Changelogs map[string]string `yaml:"dictionary"`
+}
+
+func NewDictionary(r io.Reader) (Dictionary, error) {
+	d := Dictionary{}
+
+	buf, err := io.ReadAll(r)
+	if err != nil {
+		return d, fmt.Errorf("reading dictionary from source: %w", err)
+	}
+
+	err = yaml.Unmarshal(buf, &d)
+	if err != nil {
+		return d, fmt.Errorf("unmarshaling yaml: %w", err)
+	}
+
+	return d, nil
+}
+
+func (d Dictionary) Map(dep changelog.Dependency) string {
+	if len(d.Changelogs) == 0 {
+		return ""
+	}
+
+	if tpl := d.Changelogs[dep.Name]; tpl != "" {
+		link, err := d.template(dep, tpl)
+		if err != nil {
+			log.Errorf("Error mapping changelog for %q from dictionary: %v", dep.Name, err)
+			return ""
+		}
+
+		return link
+	}
+
+	log.Debugf("Dependency %q did not match any entry in the dictionary, attempting a partial match", dep.Name)
+	for name, tpl := range d.Changelogs {
+		if !strings.Contains(dep.Name, name) {
+			continue
+		}
+
+		log.Infof("Linking changelog for %q from dictionary entry %q", dep.Name, name)
+
+		link, err := d.template(dep, tpl)
+		if err != nil {
+			log.Errorf("Error mapping changelog for %q from dictionary: %v", dep.Name, err)
+			return ""
+		}
+
+		return link
+	}
+
+	return ""
+}
+
+func (d Dictionary) template(dep changelog.Dependency, tplString string) (string, error) {
+	tpl, err := template.New("changelog").Parse(tplString)
+	if err != nil {
+		return "", fmt.Errorf("parsing template: %w", err)
+	}
+
+	linkTemplate := &strings.Builder{}
+	err = tpl.Execute(linkTemplate, dep)
+	if err != nil {
+		return "", fmt.Errorf("executing template: %w", err)
+	}
+
+	link := linkTemplate.String()
+	if strings.Contains(link, "<nil>") {
+		return "", ErrNilTemplateField
+	}
+
+	return link, nil
+}
