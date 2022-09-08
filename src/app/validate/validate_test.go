a47a87b17abe1cfce6e157c9744322498bbae9bc
alvarocabanas <acabanas@newrelic.com>
2022-09-08 09:54:53 +0200

    Add Gha output to validate command and create GH action

--- src/app/validate/validate_test.go
+++ src/app/validate/validate_test.go
@@ -10,13 +10,15 @@ import (
 	"github.com/newrelic/release-toolkit/app"
 )
 
-//nolint:paralleltest // urfave/cli cannot be tested concurrently.
+//nolint:paralleltest,funlen // urfave/cli cannot be tested concurrently.
 func TestValidate(t *testing.T) {
 	for _, tc := range []struct {
-		name     string
-		md       string
-		args     string
-		expected string
+		name        string
+		ghaArg      string
+		md          string
+		args        string
+		expectedErr string
+		expectedGha string
 	}{
 		{
 			name: "Changelog_With_Two_Errors",
@@ -40,10 +42,39 @@ Support has been removed
 - This is in the past and should not be included
 `),
 			args: "--exit-code=0",
-			expected: strings.TrimLeft(`
+			expectedErr: strings.TrimLeft(`
 "Important announcement (note)" header found with empty content
 "Breaking" header must contain only an itemized list
 `, "\n"),
+		},
+		{
+			name:   "Changelog_With_Two_Errors_GH_Action",
+			ghaArg: "-gha=1",
+			md: strings.TrimSpace(`
+# Changelog
+This is based on blah blah blah
+
+## Unreleased
+
+### Important announcement (note)
+
+### Breaking
+Support has been removed
+
+### Security
+- Fixed a security issue that leaked all data
+
+## v1.2.3 - 20YY-DD-MM
+
+### Enhancements
+- This is in the past and should not be included
+`),
+			args: "--exit-code=0",
+			expectedErr: strings.TrimLeft(`
+"Important announcement (note)" header found with empty content
+"Breaking" header must contain only an itemized list
+`, "\n"),
+			expectedGha: "::set-output name=valid::false\n",
 		},
 		{
 			name: "Valid_Changelog",
@@ -67,8 +98,35 @@ This is a release note
 ### Enhancements
 - This is in the past and should not be included
 `),
-			args:     "--exit-code=0",
-			expected: "",
+			args:        "--exit-code=0",
+			expectedErr: "",
+		},
+		{
+			name:   "Valid_Changelog_GH_Action",
+			ghaArg: "-gha=1",
+			md: strings.TrimSpace(`
+# Changelog
+This is based on blah blah blah
+
+## Unreleased
+
+### Important announcement (note)
+This is a release note
+
+### Breaking
+- Support has been removed
+
+### Security
+- Fixed a security issue that leaked all data
+
+## v1.2.3 - 20YY-DD-MM
+
+### Enhancements
+- This is in the past and should not be included
+`),
+			args:        "--exit-code=0",
+			expectedErr: "",
+			expectedGha: "::set-output name=valid::true\n",
 		},
 	} {
 		tc := tc
@@ -77,8 +135,9 @@ This is a release note
 			tDir := t.TempDir()
 
 			app := app.App()
+			bufErr := &strings.Builder{}
 			buf := &strings.Builder{}
-			app.ErrWriter = buf
+			app.ErrWriter = bufErr
 			app.Writer = buf
 
 			mdPath := path.Join(tDir, "CHANGELOG.md")
@@ -90,13 +149,17 @@ This is a release note
 
 			_, _ = mdFile.WriteString(tc.md)
 
-			err = app.Run(strings.Fields(fmt.Sprintf("rt validate -md %s %s", mdPath, tc.args)))
+			err = app.Run(strings.Fields(fmt.Sprintf("rt %s validate -md %s %s", tc.ghaArg, mdPath, tc.args)))
 			if err != nil {
 				t.Fatalf("Error running app: %v", err)
 			}
 
-			if actual := buf.String(); actual != tc.expected {
-				t.Fatalf("Expected:\n%s\n\napp printed:\n%s", tc.expected, actual)
+			if actual := bufErr.String(); actual != tc.expectedErr {
+				t.Fatalf("Expected:\n%s\n\napp printed:\n%s", tc.expectedErr, actual)
+			}
+
+			if actual := buf.String(); actual != tc.expectedGha {
+				t.Fatalf("Expected:\n%s\n\napp printed:\n%s", tc.expectedGha, actual)
 			}
 		})
 	}
