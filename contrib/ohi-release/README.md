a6d2d60f032b0b8a0525a6d278c82b26b723d4c0
Juan Manuel Perez <jperezflorez@newrelic.com>
2023-01-31 17:24:48 +0100

    Add contrib action for OHI release

---
+++ contrib/ohi-release/README.md
@@ -0,0 +1,63 @@
+# 🛠️ `ohi-release`
+
+This is a wrapper of all the steps needed to update the changelog and render a snippet to be ready to be used as a release message. This contribution also includes a script that allow to replicate what this action does locally.
+
+## Example Usage
+
+```yaml
+- uses: newrelic/release-toolkit/contrib/ohi-release@v1
+  id: release
+- name: Commit updated changelog
+  run: |
+    git add CHANGELOG.md
+    git commit -m "Update changelog with changes from ${{ steps.release.outputs.next-version }}"
+    git push -u origin main
+    gh release create ${{ steps.version.outputs.release-title }} --target $(git rev-parse HEAD) --notes-file CHANGELOG.partial.md
+```
+
+## Parameters
+
+All parameters are optional:
+  * `excluded-dirs` exclude commits whose changes only impact files in specified dirs relative to repository root. Defaults to ".github".
+  * `fail-if-held` fails if the held toggle is active
+  * `dictionary` sets the link dependency dictionary. Defaults to ".github/rt-dictionary.yaml".
+
+## Outputs
+
+  * `next-version` contains the calculated version for this.
+  * `release-title` is the title of the release that includes `next-version` and the date it was done.
+  * `release-changelog` contains the complete changelog of this release. Alias of the file `CHANGELOG.md`
+  * `release-changelog-partial` contains the changelog for only this release. Alias of the file `CHANGELOG.partial.md`
+
+This action also leaves the files `CHANGELOG.md` and `CHANGELOG.partial.md` at the working directory so they are also ready to be committed.
+
+## Use script locally
+There is a `run.sh` script that should do the same as this action: Leaves the files `CHANGELOG.md` and `CHANGELOG.partial.md` at the working directory and prints the title of the release with the next version and the date.
+
+You can run it by bashpipeing this script:
+```shell
+curl "https://github.com/newrelic/release-toolkit/blob/v1/contrib/ohi-release/run.sh" | bash
+```
+
+## Contributing
+
+Standard policy and procedure across the New Relic GitHub organization.
+
+#### Useful Links
+* [Code of Conduct](../CODE_OF_CONDUCT.md)
+* [Security Policy](../SECURITY.md)
+* [License](../LICENSE)
+
+## Support
+
+New Relic has open-sourced this project. This project is provided AS-IS WITHOUT WARRANTY OR DEDICATED SUPPORT. Issues and contributions should be reported to the project here on GitHub.
+
+We encourage you to bring your experiences and questions to the [Explorers Hub](https://discuss.newrelic.com) where our community members collaborate on solutions and new ideas.
+
+## License
+
+release-toolkit is licensed under the [Apache 2.0](http://apache.org/licenses/LICENSE-2.0.txt) License.
+
+## Disclaimer
+
+This tool is provided by New Relic AS IS, without warranty of any kind. New Relic does not guarantee that the tool will: not cause any disruption to services or systems; provide results that are complete or 100% accurate; correct or cure any detected vulnerability; or provide specific remediation advice.
---
+++ contrib/ohi-release/run.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+set -euo pipefail
+
+RT_PKG="github.com/newrelic/release-toolkit@latest"
+ARGS="$*"
+
+
+# creating a temporary folder where to build the rt binary and cleaning after exiting.
+TEMP_BIN=$(mktemp -dt release-toolkit-XXX)
+function cleanup() {
+    rm -rf $TEMP_BIN    || true
+    rm CHANGELOG.md.bak || true
+    rm changelog.yaml   || true
+}
+trap cleanup EXIT
+
+
+# usage and helm command. It is also an error exit in case flags are not correct.
+function help() {
+    set +x  # Disable verbosity. If it is enabled at this point, it is not needed anymore.
+    ERRNO=0
+    if ! [ -z "${1:-}" ]; then
+        echo "ERROR:"
+        echo "   $1"
+        echo ""
+        ERRNO=1
+    fi
+
+    cat <<EOM
+NAME:
+   $0 - release toolkit wrapper to release OHIs
+
+USAGE:
+   $0 [options]
+
+DESCRIPTION:
+   Wrapper for release toolkit that runs commands needed to release an OHI:
+    * rt validate-markdown
+    * rt generate-yaml
+    * rt is-held
+    * rt link-dependencies
+    * rt update-markdown (with the version calculated from the next-version command)
+    * rt render-changelog (with the version calculated from the next-version command)
+
+   At the end of the run, this command should output three files:
+    * CHANGELOG.md updated with the last changelog rendered (Old CHANGELOG backed up as CHANGELOG.md.bak)
+    * A CHANGELOG.partial.md with the changes for this release only.
+
+OPTIONS:
+   --verbose        Adds verbose mode to this script.
+   --help           Show this help message and exits.
+   --excluded-dirs  Exclude commits whose changes only impact files in specified dirs relative to repository root. Defaults to ".github".
+   --no-fail        Do not fail even in the held toggle is active
+   --dictionary     Sets the link dependency dictionary. Defaults to ".github/rt-dictionary.yaml".
+
+EOM
+    exit $ERRNO
+}
+
+
+# parsing flags
+EXCLUDED_DIRECTORIES=".github"
+IS_HELD_FAIL="--fail"
+DICTIONARY=".github/rt-dictionary.yaml"
+
+while true; do
+    if [ -z "${1:-}" ]; then
+        break;
+    else
+        case "${1}" in
+            -v | --verbose ) set -x; echo "Called with these arguments: $ARGS"; shift ;;
+            -h | --help ) help ;;
+            # Flags for generate-yaml
+            --excluded-dirs ) EXCLUDED_DIRECTORIES="$2"; shift 2 ;;
+            # Flags for is-held
+            --no-fail ) IS_HELD_FAIL=""; shift ;;
+            # Flags for link-dependencies
+            --dictionary ) DICTIONARY="$2"; shift 2 ;;
+            * ) help "option is not accepted/parsable: \"$1\"" ;;
+        esac
+    fi
+done
+
+
+# building rt
+GOBIN="${TEMP_BIN}" go install ${RT_PKG}
+RT_BIN="${TEMP_BIN}/release-toolkit"
+if ! [ -x $RT_BIN ]; then
+    help "rt binary is not executable: \"${RT_BIN}\""
+fi
+
+
+# checking that working directory is correctly set
+${RT_BIN} validate-markdown
+
+
+# generating the changelog
+${RT_BIN} generate-yaml --excluded-dirs "$EXCLUDED_DIRECTORIES"
+${RT_BIN} is-held ${IS_HELD_FAIL} > /dev/null
+${RT_BIN} link-dependencies
+NEXT_VERSION="$(${RT_BIN} next-version)"
+${RT_BIN} update-markdown --version "$NEXT_VERSION"
+${RT_BIN} render-changelog --version "$NEXT_VERSION"
+
+echo "Release title should be: $(grep -E "^## " CHANGELOG.partial.md | sed 's|^## ||')"
